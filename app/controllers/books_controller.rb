02914bb57f417996bd6a160c21d55468d9666b3e
EC2 Default User <ec2-user@ip-172-31-80-166.ec2.internal>
2021-07-19 13:49:45 +0000

    [Start]メッセージ機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,9 +12,11 @@ class BooksController < ApplicationController
   
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to book_path(book.id)
     
+    end
   end
 
   def edit
@@ -24,8 +26,11 @@ class BooksController < ApplicationController
   
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
+    if book.update(book_params)
+      flash[:notice] = 'Book was successfully updated'
     redirect_to book_path(book.id)
+    
+    end
     
   end
   
